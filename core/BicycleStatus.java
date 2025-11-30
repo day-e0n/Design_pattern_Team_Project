758fe6adfd8a71adf2ecf5e458eb0b1ac03e351a
heeschan <a011214@naver.com>
2025-12-01 05:15:29 +0900

    observer/state operations are totally applied to core/ConsoleInterface.java, LocationManager.java, Bicycle.java

--- core/BicycleStatus.java
+++ core/BicycleStatus.java
@@ -4,9 +4,9 @@ package core;
  * 자전거 상태 enum
  */
 public enum BicycleStatus {
-    AVAILABLE("대여가능"),
-    RENTED("대여중"),
-    MAINTENANCE("정비중"),
+    AVAILABLE("대여 가능"),
+    RENTED("대여 중"),
+    MAINTENANCE("수리 중"),
     BROKEN("고장");
     
     private String description;
